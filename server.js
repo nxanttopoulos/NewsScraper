9e6f2f8d13569ba25b44e7217953bb500e7ec8c6
Nicholas Xanttopoulos <nxanttopoulos@gmail.com>
2017-06-17 09:50:48 -0400

    fixed port environment

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ var cheerio = require("cheerio");
 mongoose.Promise = Promise;
 // Initialize Express
 var app = express();
+var PORT = process.env.PORT || 3000; 
 // Use morgan and body parser with our app
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
@@ -153,6 +154,6 @@ app.post("/articles/:id", function(req, res) {
   });
 });
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("App running on port 3000!");
 });
